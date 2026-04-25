ae1c61b27dd5c0a51827dc93457547718770a3b0
Maxon Phong <fangmingxuan@aowu.tech>
2026-04-25 22:01:46 +0800

    feat: 添加服务器动作允许的域名配置
    在next配置中启用实验性功能serverActions，并设置允许的域名来源为blog.aowu.tech和*.aowu.tech

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["blog.aowu.tech", "*.aowu.tech"],
+    },
+  },
   images: {
     remotePatterns: [
       {
